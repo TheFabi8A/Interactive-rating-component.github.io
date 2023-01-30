adbdf3b009022867dada3b093c5876553e98b069
TheFabi8A <cafaruoc2000@hotmail.com>
2023-01-30 10:05:55 -0500

    confirm

--- app.js
+++ app.js
@@ -6,27 +6,27 @@ let span = document.getElementById('rating'); // empty span where the user selec
 
 // Looping through all the number divs//
 for (let i = 0; i < ratings.length; i++) {
-          // Listening for a click event on each number div //
-          ratings[i].addEventListener('click', () => {
+    // Listening for a click event on each number div //
+    ratings[i].addEventListener('click', () => {
 
-                    // getting the clicked number value property from 1 to 5//
-                    let rating = ratings[i].innerHTML;
+        // getting the clicked number value property from 1 to 5//
+        let rating = ratings[i].innerHTML;
 
-                    // looping through the numbers to check for a previously selected rating and removing if such. //
-                    // Necessary in order to have only one rating selected at a time //
-                    for (let i = 0; i < ratings.length; i++) {
-                              ratings[i].classList.contains('selected')
-                              ratings[i].classList.remove('selected');
-                    }
+        // looping through the numbers to check for a previously selected rating and removing if such. //
+        // Necessary in order to have only one rating selected at a time //
+        for (let i = 0; i < ratings.length; i++) {
+            ratings[i].classList.contains('selected')
+            ratings[i].classList.remove('selected');
+        }
 
-                    // add the selected class which highlights the selected rating //
-                    ratings[i].classList.add('selected');
+        // add the selected class which highlights the selected rating //
+        ratings[i].classList.add('selected');
 
-                    // listening for a click on the btn in order to hide the current div and display the Thank You div with the chosen rating in the span //
-                    btn.addEventListener('click', () => {
-                              evaluation.style.display = 'none';
-                              thankyou.style.display = 'flex';
-                              span.innerHTML = rating;
-                    })
-          })
+        // listening for a click on the btn in order to hide the current div and display the Thank You div with the chosen rating in the span //
+        btn.addEventListener('click', () => {
+            evaluation.style.display = 'none';
+            thankyou.style.display = 'flex';
+            span.innerHTML = rating;
+        })
+    })
 }
